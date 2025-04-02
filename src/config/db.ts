40bc045615073608617dc376b38112731afa43d3
Tyler-Kuhn <tyler.kuhn94@outlook.com>
2025-04-02 07:16:58 -0400

    first commit

---
+++ src/config/db.ts
@@ -0,0 +1,8 @@
+// Importing the Prisma Client
+import { PrismaClient } from '@prisma/client';
+
+// Creating an instance of PrismaClient
+const prisma = new PrismaClient();
+
+// Exporting the Prisma Client instance
+export default prisma;
---
+++ src/config/server.ts
@@ -0,0 +1,37 @@
+import express, { Request, Response } from "express";
+import cors from 'cors';
+import prisma from "./db";
+import dotenv from 'dotenv';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import rateLimit from 'express-rate-limit';
+
+dotenv.config()
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+app.use(express.json());
+app.use(cors());
+app.use(morgan("dev"));
+app.use(helmet());  // Add security-related HTTP headers
+app.use(rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // limit each IP to 100 requests per windowMs
+  message: 'Too many requests, please try again later.'
+}));
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("API is running...");
+});
+
+
+app.use((err: any, req: Request, res: Response, next: Function) => {
+    console.error(err);  
+    res.status(500).json({ error: 'Something went wrong' });
+  });
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
