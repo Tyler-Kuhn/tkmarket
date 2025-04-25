75235794ae7aeb2e225b035a1fef82b63d15ee5e
Tyler-Kuhn <tyler.kuhn94@outlook.com>
2025-04-25 05:11:44 -0400

    removed dist from being tracked

--- dist/config/db.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// Importing the Prisma Client
-const client_1 = require("@prisma/client");
-// Creating an instance of PrismaClient
-const prisma = new client_1.PrismaClient();
-// Exporting the Prisma Client instance
-exports.default = prisma;
