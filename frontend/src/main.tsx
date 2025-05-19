f9366bc7839e6ca6c5f5c259014877b3aeaad8b4
Tyler-Kuhn <tyler.kuhn94@outlook.com>
2025-05-19 14:16:45 -0400

    fixed main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,9 +5,11 @@ import "./index.css";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} />
-    </Routes>
-  </BrowserRouter>
+  <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
 );
