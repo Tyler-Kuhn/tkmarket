230f18dc41cc385a134807b64dcdf831761bb18f
Tyler-Kuhn <tyler.kuhn94@outlook.com>
2025-05-19 14:15:55 -0400

    added navbar

---
+++ frontend/src/components/NavBar.tsx
@@ -0,0 +1,20 @@
+import { NavLink } from 'react-router'
+import './NavBar.css'
+
+function NavBar() {
+    return (
+        <>
+        <nav>
+            <a>TKMarket</a>
+            <ul>
+                <li><NavLink to="/app">Home</NavLink></li>
+                <li><NavLink to="/products">Products</NavLink></li>
+                <li><NavLink to="/account">Account</NavLink></li>
+                <li><NavLink to="/register">Register</NavLink></li>
+            </ul>
+        </nav>
+        </>
+    )
+}
+
+export default NavBar
