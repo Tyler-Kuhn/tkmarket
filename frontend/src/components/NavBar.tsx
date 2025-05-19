ff086cc664d5897119815c21731a3b828f8b5bcb
Tyler-Kuhn <tyler.kuhn94@outlook.com>
2025-05-19 14:17:28 -0400

    fixed navlink

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -7,7 +7,7 @@ function NavBar() {
         <nav>
             <a>TKMarket</a>
             <ul>
-                <li><NavLink to="/app">Home</NavLink></li>
+                <li><NavLink to="/">Home</NavLink></li>
                 <li><NavLink to="/products">Products</NavLink></li>
                 <li><NavLink to="/account">Account</NavLink></li>
                 <li><NavLink to="/register">Register</NavLink></li>
