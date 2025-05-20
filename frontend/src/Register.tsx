56b40e5bc1b9c70b929d9e58762a23655c4267c1
Tyler-Kuhn <tyler.kuhn94@outlook.com>
2025-05-20 11:03:36 -0400

    fixed styles

--- frontend/src/Register.tsx
+++ frontend/src/Register.tsx
@@ -88,7 +88,10 @@ export default function Register() {
               />
             </div>
 
-            <button type="submit" className="...">
+            <button
+              type="submit"
+              className="flex w-full justify-center rounded-md bg-gray-800 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-gray-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
               Register
             </button>
           </form>{" "}
