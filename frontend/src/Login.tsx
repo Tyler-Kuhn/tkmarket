13758b78103b73f37e297eb3e66207eed24b68f6
Tyler-Kuhn <tyler.kuhn94@outlook.com>
2025-05-20 15:36:24 -0400

    fixed function name

--- frontend/src/Login.tsx
+++ frontend/src/Login.tsx
@@ -2,7 +2,7 @@ import NavBar from "./components/NavBar";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-export default function Register() {
+export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
