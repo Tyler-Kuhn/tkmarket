7a64ada0ae1ce0dc36a04533af10bedf90a48c24
Tyler-Kuhn <tyler.kuhn94@outlook.com>
2025-05-20 05:52:23 -0400

    Register page finished

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,12 +3,17 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router";
 import "./index.css";
 import App from "./App.tsx";
+import Register from "./Register.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
+        <Route path="/products" element={<App />} />
+        <Route path="/cart" element={<App />} />
+        <Route path="/login" element={<App />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
