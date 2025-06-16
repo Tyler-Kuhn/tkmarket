d7bc48f3e4f1a82edc178b6a5c345b817c71cdad
Tyler-Kuhn <tyler.kuhn94@outlook.com>
2025-06-16 11:25:07 -0400

    fixed button coloring

--- frontend/src/Account.tsx
+++ frontend/src/Account.tsx
@@ -265,7 +265,7 @@ export default function Account() {
 
               <button
                 type="submit"
-                className="w-full rounded-md bg-green-600 py-2 px-4 text-white hover:bg-green-500"
+                className="flex w-full justify-center rounded-md bg-gray-800 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-gray-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
                 {editingAddress ? "Update Address" : "Add Address"}
               </button>
@@ -330,7 +330,7 @@ export default function Account() {
               </div>
               <button
                 type="submit"
-                className="w-full rounded-md bg-indigo-600 py-2 px-4 text-white hover:bg-indigo-500"
+                className="flex w-full justify-center rounded-md bg-gray-800 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-gray-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
                 Update Info
               </button>
